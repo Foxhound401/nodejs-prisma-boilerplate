dbc6380c7e50a37c2f90f2c7608235b432db129b
Phuc Phung <phungthienphuc18@gmail.com>
2022-03-01 15:35:30 +0700

    feat: update routes sso

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,11 @@
-const {
-    Router
-} = require('express');
-const userRoutes = require('./users');
-const authRoutes = require('./auth');
+const { Router } = require("express");
+const userRoutes = require("./users");
+const authRoutes = require("./auth");
 const router = Router();
 
-router.get('/', (req, res) => res.send("hello sso"));
+router.get("/v1/", (req, res) => res.send("hello sso"));
 
-router.use('/users', userRoutes);
-router.use('/', authRoutes);
+router.use("/v1/users", userRoutes);
+router.use("/v1/", authRoutes);
 
 module.exports = router;
